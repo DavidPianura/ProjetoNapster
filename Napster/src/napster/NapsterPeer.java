1400a8aeea07ee222da1229a5eaf11124095e3b0
David Pianura <82898621+DavidPianura@users.noreply.github.com>
2022-07-26 21:24:41 -0300

    Funcionalidades Implementadas

--- Napster/src/napster/NapsterPeer.java
+++ Napster/src/napster/NapsterPeer.java
@@ -12,8 +12,12 @@ import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
@@ -30,43 +34,68 @@ public class NapsterPeer {
 	// Definindo uma porta para o Peer
 	static int myPort = rngNumber(9000, 8000);
 	
+	// Variavel para armazenar qual a porta onde o Socket da Thread para responder as requisicoes de ALIVE estara
+	static int alivePort = rngNumber(7000, 6000);
+	
 	// Thread para responder a requisicao ALIVE do server
 	public static class ThreadAlive extends Thread {
 		
 		private DatagramSocket clientSocket;
+		private InetAddress IPAddres;
+		
+		public ThreadAlive(InetAddress IPAddres, DatagramSocket aliveSocket) throws SocketException {
+			
+			/*
+			Assim que a Thread para responder requisicoes de ALIVE eh iniciada, a variavel global alivePort
+			recebe o valor do socket exclusivo para responder essas solicitacoes
+			*/
+			this.clientSocket = aliveSocket;
+			this.IPAddres = IPAddres;
 
-		public ThreadAlive(DatagramSocket clientSocket) {
-			this.clientSocket = clientSocket;
 		}
 		
 		public void run() {
-			try {
-				InetAddress IPAddres = InetAddress.getByName("127.0.0.1");
+	
 				Gson gson = new Gson();
 				byte[] sendBuf = new byte[1024];
-				sendBuf = "ALIVE_OK".getBytes();
+				
+				// LinkedList<String> udpPort = new LinkedList<String>();
+				
+				// Envia a porta que o DatagramSocket original esta enviando as mensagens
+				// udpPort.add(Integer.toString(this.port_udp));
+				
+				NapsterMessage aliveOK = new NapsterMessage("ALIVE_OK", null, myPort, alivePort);
+				sendBuf = gson.toJson(aliveOK).getBytes();
+				
+				DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length, IPAddres, 10098);
+				
+				byte[] recBuffer = new byte[1024];
+				DatagramPacket recPkt = new DatagramPacket(recBuffer, recBuffer.length);
 				
 				while (true) {
-					byte[] recBuffer = new byte[1024];
-					
-					DatagramPacket recPkt = new DatagramPacket(recBuffer, recBuffer.length);
-					
-					// Aguardando o ALIVE do servidor
-					clientSocket.receive(recPkt); // BLOCKING
-					
-					String resposta = new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength());
-					NapsterMessage aliveRequest = gson.fromJson(resposta, NapsterMessage.class);
-					
-					if (aliveRequest.getRequest().equals("ALIVE")) {
+					try {
+	
+						// Aguardando o ALIVE do servidor
+						this.clientSocket.receive(recPkt); // BLOCKING
+	
+						String resposta = new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength());
+						NapsterMessage aliveRequest = gson.fromJson(resposta, NapsterMessage.class);
 						
-						DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length, IPAddres, 10098);
-						clientSocket.send(sendPacket);
+						if (aliveRequest.getRequest().equals("ALIVE")) {
+							clientSocket.send(sendPacket);
+
+						} else {
+							recBuffer = new byte[1024];
+							recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+							continue;
+						}
+						
+					} catch (Exception e) {
+						// TODO: handle exception
 					}
-					
+							
 				}
-			}catch (Exception e) {
 				
-			}
 		}
 	}
 	
@@ -180,11 +209,11 @@ public class NapsterPeer {
 								
 					rand = rngNumber(1, 10);
 					
-					// Aceita ou rejeita o Download de forma aleatória
+					// Aceita ou rejeita o Download de forma aleatï¿½ria
 					// Nesse caso, se o numero aleatorio for impar, a solicitacao de download eh rejeitada
 					if (rand % 2 != 0) {
 							
-						NapsterMessage downloadNegado = new NapsterMessage("DOWNLOAD_NEGADO", this.file_request, myPort);
+						NapsterMessage downloadNegado = new NapsterMessage("DOWNLOAD_NEGADO", this.file_request, myPort, alivePort);
 						sendResponse(no, gson.toJson(downloadNegado));
 					
 					} else {
@@ -192,7 +221,7 @@ public class NapsterPeer {
 						File myFile = new File (this.diretorio + "\\" + this.file_request.get(0));
 						int tamanho = (int)myFile.length();
 
-						NapsterMessage downloadAceito = new NapsterMessage("DOWNLOAD_ACEITO", this.file_request, tamanho);
+						NapsterMessage downloadAceito = new NapsterMessage("DOWNLOAD_ACEITO", this.file_request, myPort, alivePort);
 						
 						// Avisa o Peer que o Download foi aceito
 						sendResponse(no, gson.toJson(downloadAceito));
@@ -202,7 +231,7 @@ public class NapsterPeer {
 						
 				} else {
 						
-					NapsterMessage downloadNegado = new NapsterMessage("DOWNLOAD_NEGADO", this.file_request, myPort);
+					NapsterMessage downloadNegado = new NapsterMessage("DOWNLOAD_NEGADO", this.file_request, myPort, alivePort);
 					sendResponse(no, gson.toJson(downloadNegado));
 					
 				}
@@ -249,7 +278,7 @@ public class NapsterPeer {
 	// Metodo para fazer a Request de Download aos peers que possuem o arquivo desejado
 	public static void downloadRequest(LinkedList<String> peers_with_file, LinkedList<String> file_request, String folder) {
 		boolean dAccept = false;
-		NapsterMessage dRequest = new NapsterMessage("DOWNLOAD", file_request, myPort);
+		NapsterMessage dRequest = new NapsterMessage("DOWNLOAD", file_request, myPort, alivePort);
 		Gson gson = new Gson();
 		int rejeitados = 0;
 		
@@ -305,7 +334,10 @@ public class NapsterPeer {
 		}
 	}
 	
+	// Funcao para dar Join ao servidor
 	public static void join(NapsterMessage msg, String req, InetAddress IPAddres, DatagramSocket clientSocket) throws IOException {
+		boolean received = false;
+		
 		byte[] sendData_join = new byte[1024];
 		Gson gson = new Gson();
 		
@@ -315,84 +347,162 @@ public class NapsterPeer {
 		
 		byte[] recBuffer = new byte[1024];	
 		DatagramPacket recPkt = new DatagramPacket(recBuffer, recBuffer.length);
-		clientSocket.receive(recPkt);
 		
-		String informacao = new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength());
-		
-		// Printando infos quando JOIN_OK eh recebido
-		NapsterMessage joinOK = gson.fromJson(informacao, NapsterMessage.class);
-		InetAddress peer_ip = InetAddress.getLocalHost();
-		
-		if (joinOK.getRequest().equals("JOIN_OK"))
-			System.out.println("Sou o peer " + peer_ip.getLoopbackAddress().getHostAddress() + ":" + myPort + " com arquivos " + msg.getProperties());
-		
+		// Tenta receber a mensagem do servidor, caso o Socket chegue no timeout definido, a mensagem é reenviada
+		while (!received) {
+			try {
+				clientSocket.receive(recPkt);
+			    String informacao = new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength());
+			    NapsterMessage joinOK = gson.fromJson(informacao, NapsterMessage.class);
+			    
+			    if (joinOK.getRequest().equals("JOIN_OK")) {
+					// Printando infos quando JOIN_OK eh recebido
+					InetAddress peer_ip = InetAddress.getLocalHost();
+					System.out.println("Sou o peer " + peer_ip.getLoopbackAddress().getHostAddress() + ":" + myPort + " com arquivos " + msg.getProperties());
+			    	received = true; 
+			    }  else {
+					recBuffer = new byte[1024];	
+					recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+					continue;
+			    }
+			    
+			} catch (SocketTimeoutException e) {
+				// tentando reenviar a mensagem
+				recBuffer = new byte[1024];	
+				recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+				clientSocket.send(sendPacket);
+				continue;
+			}
+		}
+
 	}
 	
+	// funcao para envio do leave
 	public static void leave(NapsterMessage msg_leave, String req, InetAddress IPAddres, DatagramSocket clientSocket) throws IOException {
-		byte[] sendData_leave = new byte[1024];
 		
+		boolean received = false;
+		
+		byte[] sendData_leave = new byte[1024];
 		Gson gson = new Gson();
 		
 		sendData_leave = req.getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData_leave, sendData_leave.length, IPAddres, 10098);
-		clientSocket.send(sendPacket);
 		
 		byte[] recBuffer = new byte[1024];	
 		DatagramPacket recPkt_leave = new DatagramPacket(recBuffer, recBuffer.length);
-		clientSocket.receive(recPkt_leave);
 		
-		String informacao = new String(recPkt_leave.getData(), recPkt_leave.getOffset(), recPkt_leave.getLength());
+		clientSocket.send(sendPacket);
 		
-		NapsterMessage leaveOK = gson.fromJson(informacao, NapsterMessage.class);
-		
-		if (leaveOK.getRequest().equals("LEAVE_OK")) {
-			
+		// Tenta receber a mensagem de Leave. Caso nao receba ate o timeout do Socket, reenvia a mensagem
+		while (!received) {
+			try {
+				clientSocket.receive(recPkt_leave);
+				
+				String informacao = new String(recPkt_leave.getData(), recPkt_leave.getOffset(), recPkt_leave.getLength());
+				NapsterMessage leaveOK = gson.fromJson(informacao, NapsterMessage.class);
+				
+				if (leaveOK.getRequest().equals("LEAVE_OK")) {
+					received = true;
+					System.out.println("SAINDO...");
+				}
+				else {
+					recBuffer = new byte[1024];	
+					recPkt_leave = new DatagramPacket(recBuffer, recBuffer.length);
+					continue;
+				}
+					
+			} catch (SocketTimeoutException e) {
+				recBuffer = new byte[1024];	
+				recPkt_leave = new DatagramPacket(recBuffer, recBuffer.length);
+				clientSocket.send(sendPacket);
+				continue;
+			}
 		}
-			
-		
+
 	}
 	
 	public static LinkedList<String> search(NapsterMessage msg, String req, InetAddress IPAddres, DatagramSocket clientSocket) throws IOException {
+		
+		boolean received = false;
 		
 		Gson gson = new Gson();
 		byte[] sendData = new byte[1024];
 		sendData = req.getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddres, 10098);
-		clientSocket.send(sendPacket);
+		
+		LinkedList<String> peers_with_file = new LinkedList<String>();
 		
 		byte[] recBuffer = new byte[1024];	
 		DatagramPacket recPkt = new DatagramPacket(recBuffer, recBuffer.length);
-		clientSocket.receive(recPkt);
 		
-		String informacao = new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength());
-		
-		System.out.println("peers com arquivo solicitado: " + gson.fromJson(informacao, LinkedList.class));
-		
-		return gson.fromJson(informacao, LinkedList.class);
-		
+		clientSocket.send(sendPacket);
+
+		// Tenta receber o SEARCH_OK. Caso não receba, tenta reenviar novamente
+		while (!received) {
+			try {
+				clientSocket.receive(recPkt);
+	
+				String informacao = new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength());
+				NapsterMessage searchOK = gson.fromJson(informacao, NapsterMessage.class);
+				
+				if (searchOK.getRequest().equals("SEARCH_OK")) {
+	
+					for (int i = 0; i < searchOK.getProperties().size(); i++) 
+						peers_with_file.add(searchOK.getProperties().get(i));
+					
+					System.out.println("peers com arquivo solicitado: " + peers_with_file);
+					received = true;
+					
+				} else {
+					recBuffer = new byte[1024];	
+					recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+					clientSocket.send(sendPacket);
+					continue;
+				}
+			
+			} catch (SocketTimeoutException e) {
+				recBuffer = new byte[1024];	
+				recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+				clientSocket.send(sendPacket);
+				continue;
+			}
+		}
+
+		return peers_with_file;		
 	}
 	
-	public static void update (NapsterMessage msg, String req, InetAddress IPAddres, DatagramSocket clientSocket) {
-		try {
-			byte[] sendData_update = new byte[1024];
-			Gson gson = new Gson();
+	public static void update (NapsterMessage msg, String req, InetAddress IPAddres, DatagramSocket clientSocket) throws IOException {
+		
+		boolean received = false;
+		byte[] sendData_update = new byte[1024];
+		
+		Gson gson = new Gson();
+		
+		sendData_update = req.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData_update, sendData_update.length, IPAddres, 10098);
+		
+		byte[] recBuffer = new byte[1024];	
+		DatagramPacket recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+		
+		clientSocket.send(sendPacket);
+		
+		while (!received) {
+			try {
+				clientSocket.receive(recPkt);
+				
+				String informacao = new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength());
+				NapsterMessage updateOK = gson.fromJson(informacao, NapsterMessage.class);
+				
+				if (updateOK.getRequest().equals("UPDATE_OK")) received = true;
 			
-			sendData_update = req.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData_update, sendData_update.length, IPAddres, 10098);
-			clientSocket.send(sendPacket);
-			
-			byte[] recBuffer = new byte[1024];	
-			DatagramPacket recPkt = new DatagramPacket(recBuffer, recBuffer.length);
-			clientSocket.receive(recPkt);
-			
-			String informacao = new String(recPkt.getData(), recPkt.getOffset(), recPkt.getLength());
-
-			NapsterMessage updateOK = gson.fromJson(informacao, NapsterMessage.class);
-			System.out.println("UPDATE_OK!");
-			
-		} catch (Exception e) {
-			
+			} catch (SocketTimeoutException e) {
+				recBuffer = new byte[1024];	
+				recPkt = new DatagramPacket(recBuffer, recBuffer.length);
+				clientSocket.send(sendPacket);
+				continue;
+			}	
 		}
+
 	}
 	
 	// Metodo para criar a lista de arquivos do Peer
@@ -412,12 +522,21 @@ public class NapsterPeer {
 	
 	public static void main(String[] args) throws Exception{
 		
-		
-		
 		Gson gson = new Gson();
 		DatagramSocket clientSocket = new DatagramSocket();
+		DatagramSocket aliveSocket = new DatagramSocket(null);
+		
+		
+		// Variaveis de controle para o menu interativo
+		boolean joinDone = false;
+
+		// Tempo de espera por uma resposta do servidor = 30 segundos. Caso exceda esse tempo, uma nova requisicao eh enviada
+		clientSocket.setSoTimeout(15000);
 		
 		InetAddress IPAddres = InetAddress.getByName("127.0.0.1");
+
+		SocketAddress sockaddr = new InetSocketAddress(IPAddres, alivePort);  
+		aliveSocket.bind(sockaddr);
 		
 		boolean l = false;
 		Scanner tec = new Scanner (System.in);
@@ -428,53 +547,74 @@ public class NapsterPeer {
 		
 		String folder = "";
 		
-		ThreadAlive alive = new ThreadAlive(clientSocket);
+		ThreadAlive alive = new ThreadAlive(IPAddres, aliveSocket);
 		
+		// MENU INTERATIVO
 		System.out.println("----------------------------------------------");
 		System.out.println("              Projeto Napster                 ");
 		System.out.println("----------------------------------------------\n");
+		
+		System.out.println("Nome: David Pianura da Silva");
+		System.out.println("RA: 11201810096");
+		System.out.println("Professor: Vladimir Emiliano Moreira Rocha\n");
+		
+		System.out.println("Para escolher uma funcao, digite o numero dela e tecle enter:");
+		System.out.println("1. JOIN");
+		System.out.println("2. SEARCH");
+		System.out.println("3. DOWNLOAD");
+		System.out.println("4. UPDATE");
+		System.out.println("5. LEAVE");
 		
 		
 		while (!l) {
 			
 			int option = 0;
 			
-			// MENU INTERATIVO
-			System.out.println();
-			System.out.println("Para escolher uma funcao, digite o numero e tecle enter:");
-			System.out.println("1. JOIN");
-			System.out.println("2. SEARCH");
-			System.out.println("3. DOWNLOAD");
-			System.out.println("4. UPDATE");
-			System.out.println("5. LEAVE");
-			
 			while (option <= 0 || option > 5) {
-				System.out.print("Opcao escolhida: ");
+				System.out.print("\nOpcao escolhida: ");
 				option = tec.nextInt();
 				
 				if (option <= 0 || option > 5) {
-					System.out.println("Opcao invalida");
+					System.out.println("Opcao invalida.");
 				}
 			}
 			
 			switch(option) {
 				case 1:
-					System.out.print("Escreva o caminho completo da pasta: ");
+					
+					if (joinDone) {
+						System.out.println("Join ja realizado!");
+						continue;
+					}
+					
+					System.out.print("Escreva o caminho completo da pasta (e.g.: C:\\Videos\\PeerA): ");
 					tec.nextLine();
+					
 					folder = tec.nextLine();
 					files = getInfosFolder(folder);
-					NapsterMessage msg = new NapsterMessage("JOIN", files, myPort);
+					
+					NapsterMessage msg = new NapsterMessage("JOIN", files, myPort, alivePort);
+					
+					ThreadDownloadListener listener = new ThreadDownloadListener(files, folder);
+
 					join(msg, gson.toJson(msg), IPAddres, clientSocket);
 					
-					// Assim que da join no servidor e define a sua pasta, a Thread para responder
-					// requisicoes de outros peers eh iniciada
-					ThreadDownloadListener listener = new ThreadDownloadListener(files, folder);
+					// Assim que da join no servidor e define a sua pasta, a Thread para responder requisicoes de outros peers eh iniciada
+					// assim tambem como a thread para responder as requisicoes de ALIVE oriundas do servidor
 					listener.start();
-					// alive.start();
-				
+					alive.start();
+					
+					joinDone = true;
+					
 					break;
 				
 				case 2:
+					
+					if (!joinDone) {
+						System.out.println("Realize o Join primeiramente!");
+						continue;
+					}
+					
 					tec.nextLine();
 					System.out.print("Qual o arquivo a ser pesquisado?: ");
 					String arquivo = tec.nextLine();
@@ -486,11 +626,15 @@ public class NapsterPeer {
 					}
 
 					file_request.add(arquivo);
-					NapsterMessage pesquisa = new NapsterMessage("SEARCH", file_request, myPort);
+					NapsterMessage pesquisa = new NapsterMessage("SEARCH", file_request, myPort, alivePort);
 					peers_with_file = search(pesquisa, gson.toJson(pesquisa), IPAddres, clientSocket);
 					break;
 				
 				case 3:
+					if (!joinDone) {
+						System.out.println("Realize o Join primeiramente!");
+						continue;
+					}
 					if (peers_with_file.isEmpty()) {
 						System.out.println("Realize primeiro uma acao de SEARCH para buscar o arquivo");
 					} else {
@@ -499,18 +643,27 @@ public class NapsterPeer {
 					break;
 					
 				case 4:
+					if (!joinDone) {
+						System.out.println("Realize o Join primeiramente!");
+						continue;
+					}
 					files.clear();
 					files = getInfosFolder(folder);
-					NapsterMessage msgUpdate = new NapsterMessage("UPDATE", files, myPort);
+					NapsterMessage msgUpdate = new NapsterMessage("UPDATE", files, myPort, alivePort);
 					update(msgUpdate, gson.toJson(msgUpdate), IPAddres, clientSocket);
 					
 					break;
 				case 5:
-					NapsterMessage msg_leave = new NapsterMessage("LEAVE", null, myPort);
+					if (!joinDone) {
+						l = true;
+						break;
+					}
+					
+					NapsterMessage msg_leave = new NapsterMessage("LEAVE", null, myPort, alivePort);
 					leave(msg_leave, gson.toJson(msg_leave), IPAddres, clientSocket);
 					l = true;
+					System.exit(0);
 					break;
-				
 			}
 		}
 		clientSocket.close();
---
+++ TCP_UDP_Pratica/ConnDemo/src/teste.java
@@ -0,0 +1,9 @@
+
+public class teste {
+
+	public static void main(String[] args) {
+		int x = 10;
+
+	}
+
+}
